3e114a4dfdcbaf508aee738b82b15633e1df7499
Santhosh Reddy Nalabolu <santhoshreddy@Santhoshs-MacBook-Air.local>
2026-02-25 20:13:19 +0530

    Mac to windows

--- src/utils/data_loader.py
+++ src/utils/data_loader.py
@@ -2,3 +2,25 @@
 Data Loading and Preprocessing
 Handles MNIST and Fashion-MNIST datasets
 """
+import numpy as np
+from keras.datasets import mnist, fashion_mnist
+from sklearn.model_selection import train_test_split
+
+def load_data(dataset):
+    if dataset == 'mnist':
+        return mnist.load_data()
+    elif dataset == 'fashion_mnist':
+        return fashion_mnist.load_data()
+
+def process_data(x_train, x_test, y_train, y_test, val_size = 0.1):
+    x_train = x_train.flatten()
+    x_test = x_test.flattten()
+
+    sum = sum(x_train)
+    x_train = x_train / sum
+    sum = sum(x_test)
+    x_test = x_test / sum
+
+    
+    return x_train, x_test, y_train, y_test, x_val, y_val
+
