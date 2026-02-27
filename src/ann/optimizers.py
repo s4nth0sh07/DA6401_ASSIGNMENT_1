d0ac06d2f035224fd9934318655b232151d924c6
Santhosh Reddy Nalabolu <santhoshreddy@Santhoshs-MacBook-Air.local>
2026-02-27 09:11:36 +0530

    morning

--- src/ann/optimizers.py
+++ src/ann/optimizers.py
@@ -5,7 +5,7 @@ Implements: SGD, Momentum, Adam, Nadam, etc.
 import numpy as np
 
 class SGD:
-    def __init__(self, neural_layers, learning_rate)
+    def __init__(self, neural_layers, learning_rate):
         self.layers = neural_layers
         self.learning_rate = learning_rate
     
@@ -26,11 +26,14 @@ class Momentum:
     
     def update(self):
         for name, layer in self.layers.items():
-            curr_vel_w = self.b * self.v_w[name] + self.learning_rate * layer.grad_W
-            curr_vel_b = self.b * self.v_b[name] + self.learning_rate * layer.grad_b
+            curr_vel_w = self.b * self.velocity_weights[name] + self.learning_rate * layer.grad_W
+            curr_vel_b = self.b * self.velocity_biases[name] + self.learning_rate * layer.grad_b
 
             layer.weights = layer.weights - curr_vel_w
             layer.biases = layer.biases - curr_vel_b
+
+            self.velocity_biases[name] = curr_vel_b
+            self.velocity_weights[name] = curr_vel_w
 
 class RMSprop:
     def __init__(self, neural_layers, b, e, learning_rate):
@@ -44,23 +47,134 @@ class RMSprop:
 
     def update(self):
         for name, layer in self.layers.items():
-            curr_sg_w = self.b * self.v_w[name] + (1 - self.b) * (layer.grad_w ** 2)
-            curr_sg_b = self.b * self.v_b[name] + (1 - self.b) * (layer.grad_b ** 2)
+            curr_sg_w = self.b * self.squared_grad_weights[name] + (1 - self.b) * (layer.grad_W ** 2)
+            curr_sg_b = self.b * self.squared_grad_biases[name] + (1 - self.b) * (layer.grad_b ** 2)
 
-            temp = (self.learning_rate / np.sqrt(curr_sg_w) + self.e)
+            temp = self.learning_rate / (np.sqrt(curr_sg_w) + self.e)
             layer.weights = layer.weights - temp * layer.grad_W
-            temp = (self.learning_rate / np.sqrt(curr_sg_b) + self.e)
+            temp = self.learning_rate / (np.sqrt(curr_sg_b) + self.e)
             layer.biases = layer.biases - temp * layer.grad_b
 
             self.squared_grad_biases[name] = curr_sg_b
             self.squared_grad_weights[name] = curr_sg_w
 
 class Adam:
-    def __init__(self, neural_layers, b1, b2, epsilon, learning_rate):
+    def __init__(self, neural_layers, b1, b2, e, learning_rate):
         self.layers = neural_layers
         self.learning_rate = learning_rate
 
-        self.
+        self.momentum_weights = {name : np.zeros_like(layer.weights) for name, layer in self.layers.items()}
+        self.velocity_weights = {name : np.zeros_like(layer.weights) for name, layer in self.layers.items()}
+
+        self.momentum_biases = {name : np.zeros_like(layer.biases) for name, layer in self.layers.items()}
+        self.velocity_biases = {name : np.zeros_like(layer.biases) for name, layer in self.layers.items()}
+
         self.b1, self.b2 = b1, b2
-        self.epsilon = epsilon
+        self.e = e
         self.counter = 0
+
+    def update(self):
+        self.counter += 1
+        for name, layer in self.layers.items():
+            curr_m_w = (self.b1 * self.momentum_weights[name]) + (1 - self.b1) * layer.grad_W
+            curr_v_w = (self.b2 * self.velocity_weights[name]) + (1 - self.b2) * (layer.grad_W ** 2)
+
+            temp1 = (1 - self.b1 ** self.counter)
+            temp2 = (1 - self.b2 ** self.counter)
+
+            curr_m_w_cap = curr_m_w / temp1
+            curr_v_w_cap = curr_v_w / temp2
+
+            temp = self.learning_rate * curr_m_w_cap / (np.sqrt(curr_v_w_cap) + self.e)
+            layer.weights = layer.weights - temp
+
+            self.momentum_weights[name] = curr_m_w
+            self.velocity_weights[name] = curr_v_w
+
+            curr_m_b = (self.b1 * self.momentum_biases[name]) + (1 - self.b1) * layer.grad_b
+            curr_v_b = (self.b2 * self.velocity_biases[name]) + (1 - self.b2) * (layer.grad_b ** 2)
+
+            temp1 = (1 - self.b1 ** self.counter)
+            temp2 = (1 - self.b2 ** self.counter)
+
+
+            curr_m_b_cap = curr_m_b / temp1
+            curr_v_b_cap = curr_v_b / temp2
+
+            temp = self.learning_rate * curr_m_b_cap / (np.sqrt(curr_v_b_cap) + self.e)
+            layer.biases = layer.biases - temp
+
+            self.momentum_biases[name] = curr_m_b
+            self.velocity_biases[name] = curr_v_b
+
+class NAG:
+    def __init__(self, neural_layers, learning_rate, b):
+        self.layers = neural_layers
+        self.learning_rate = learning_rate
+
+        self.velocity_weights = {name : np.zeros_like(layer.weights) for name, layer in self.layer.items()}
+        self.velocity_biases = {name : np.zeros_like(layer.biases) for name, layer in self.layer}
+
+        self.b = b
+    
+    def update(self):
+        for name, layer in self.layers.items():
+            curr_vel_w = self.b * self.velocity_weights[name] + self.learning_rate * layer.grad_W
+            curr_vel_b = self.b * self.velocity_biases[name] + self.learning_rate * layer.grad_b
+            layer_weights = layer.weights - (self.b * curr_vel_w + self.learning_rate * layer.grad_W)
+            layer_biases = layer.biases - (self.b * curr_vel_b + self.learning_rate * layer.grad_b)
+
+            self.velocity_weights[name] = curr_vel_w
+            self.velocity_biases[name] = curr_vel_b
+
+class Nadam:
+    def __init__(self, neural_layers, b1, b2, e, learning_rate):
+        self.layers = neural_layers
+        self.learning_rate = learning_rate
+
+        self.momentum_weights = {name: np.zeros_like(layer.weights) for name, layer in self.layers.items()}
+        self.velocity_weights = {name: np.zeros_like(layer.weights) for name, layer in self.layers.items()}
+
+        self.momentum_biases = {name: np.zeros_like(layer.biases) for name, layer in self.layers.items()}
+        self.velocity_biases = {name: np.zeros_like(layer.biases) for name, layer in self.layers.items()}
+
+        self.b1, self.b2 = b1, b2
+        self.e = e
+        self.counter = 0
+
+    def update(self):
+        self.counter += 1
+        for name, layer in self.layers.items():
+            # --- WEIGHTS ---
+            curr_m_w = (self.b1 * self.momentum_weights[name]) + (1 - self.b1) * layer.grad_W
+            curr_v_w = (self.b2 * self.velocity_weights[name]) + (1 - self.b2) * (layer.grad_W ** 2)
+
+            # Bias correction
+            curr_m_w_cap = curr_m_w / (1 - self.b1 ** self.counter)
+            curr_v_w_cap = curr_v_w / (1 - self.b2 ** self.counter)
+
+            # Nesterov tweak on the momentum cap
+            m_w_nesterov = (self.b1 * curr_m_w_cap) + ((1 - self.b1) * layer.grad_W) / (1 - self.b1 ** self.counter)
+
+            temp_w = self.learning_rate * m_w_nesterov / (np.sqrt(curr_v_w_cap) + self.e)
+            layer.weights = layer.weights - temp_w
+
+            self.momentum_weights[name] = curr_m_w
+            self.velocity_weights[name] = curr_v_w
+
+            # --- BIASES ---
+            curr_m_b = (self.b1 * self.momentum_biases[name]) + (1 - self.b1) * layer.grad_b
+            curr_v_b = (self.b2 * self.velocity_biases[name]) + (1 - self.b2) * (layer.grad_b ** 2)
+
+            curr_m_b_cap = curr_m_b / (1 - self.b1 ** self.counter)
+            curr_v_b_cap = curr_v_b / (1 - self.b2 ** self.counter)
+
+            # Nesterov tweak on the bias cap
+            m_b_nesterov = (self.b1 * curr_m_b_cap) + ((1 - self.b1) * layer.grad_b) / (1 - self.b1 ** self.counter)
+
+            temp_b = self.learning_rate * m_b_nesterov / (np.sqrt(curr_v_b_cap) + self.e)
+            layer.biases = layer.biases - temp_b
+
+            self.momentum_biases[name] = curr_m_b
+            self.velocity_biases[name] = curr_v_b
+                 
